9528e322f995f005e3742aef1492787fb342804e
Renan.T <rtoniolo4@gmail.com>
2024-01-25 18:39:44 -0300

    feat: action auth

---
+++ app/lib/actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { z } from "zod";
+import { getUser } from "./data";
+import bcrypt from "bcrypt";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  const data = Object.fromEntries(formData);
+  const parsedCredentials = z
+    .object({ email: z.string().email(), password: z.string().min(6) })
+    .safeParse(data);
+
+  if (!parsedCredentials.success) return "Credencias invalida";
+
+  const { email, password } = parsedCredentials.data;
+  const user = await getUser(email);
+
+  if (!user) return "Usuario não registrado";
+
+  const passwordsMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordsMatch) return "Credencias invalida";
+
+  return "true";
+}
--- app/ui/form-login.tsx
+++
@@ -1,9 +0,0 @@
-export default function FormLogin() {
-  return (
-    <form>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>submit</button>
-    </form>
-  );
-}
