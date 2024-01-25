961f0f300901e67177848ac66b2e31707a0a3f59
Renan.T <rtoniolo4@gmail.com>
2024-01-25 18:39:00 -0300

    feat: form with formstate

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,27 @@
-import FormLogin from "../ui/form-login";
+"use client";
+
+import { useFormState, useFormStatus } from "react-dom";
+import { authenticate } from "../lib/actions";
 
 export default function Page() {
-  return <FormLogin />;
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
+  return (
+    <form action={dispatch}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <div>{errorMessage && <p>{errorMessage}</p>}</div>
+      <LoginButton />
+    </form>
+  );
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button aria-disabled={pending} type="submit">
+      Login
+    </button>
+  );
 }
