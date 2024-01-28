be966e8da4cda09a019c6aa246c461dcf8269bb9
Renan.T <rtoniolo4@gmail.com>
2024-01-28 20:19:59 -0300

    feat: created Subscribe

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+
+import { useFormState, useFormStatus } from "react-dom";
+import { createAccount } from "../lib/actions";
+
 export default function Page() {
-  return <div>testing...</div>;
+  const [errorMessage, dispatch] = useFormState(createAccount, undefined);
+
+  return (
+    <form action={dispatch}>
+      <input type="text" name="name" placeholder="Name" required />
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <input
+        type="password"
+        name="confirmed_password"
+        placeholder="Confirmed Password"
+        required
+      />
+      <div>{errorMessage && <p>{errorMessage}</p>}</div>
+      <LoginButton />
+    </form>
+  );
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button aria-disabled={pending} type="submit">
+      Create
+    </button>
+  );
 }
