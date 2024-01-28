2da75e0d71032716259b78cd83238d44578eae01
Renan.T <rtoniolo4@gmail.com>
2024-01-28 20:18:38 -0300

    feat: add btn for profile page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center gap-5 p-24">
+      <button onClick={() => router.push("/profile")}>Profile</button>
       <button onClick={() => router.push("/login")}>Login</button>
       <button onClick={() => router.push("/create")}>Create Account</button>
     </main>
