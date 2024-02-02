c22a3f4581acf881e20ae558578c9ea2395c45e5
Renan.T <rtoniolo4@gmail.com>
2024-02-02 19:53:11 -0300

    refactor: :recycle: small refector

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,24 +1,25 @@
 import { cookies } from "next/headers";
 import { type NextRequest } from "next/server";
 import { SignJWT, jwtVerify } from "jose";
+import { User } from "./definitions";
 
 export async function verifyAuth(req: NextRequest) {
   const access_token = req.cookies.get("access_token")?.value;
 
   if (!access_token) throw new Error("Sem token de acesso");
 
-  try {
-    const secret = process.env.JWT_SECRET;
-    await jwtVerify(access_token, new TextEncoder().encode(secret));
-    return true;
-  } catch (error) {
-    throw error;
-  }
+  const secret = process.env.JWT_SECRET;
+  return await jwtVerify(access_token, new TextEncoder().encode(secret)).catch(
+    (err) => {
+      throw new Error(err);
+    }
+  );
 }
 
-export async function setUserCookie() {
+export async function setUserCookie(user: User) {
   const secret = process.env.JWT_SECRET;
-  const token = await new SignJWT({})
+  const { name, email } = user;
+  const token = await new SignJWT({ name, email })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
     .setExpirationTime("7d")
@@ -26,6 +27,7 @@ export async function setUserCookie() {
 
   cookies().set("access_token", token, {
     httpOnly: true,
+    secure: true,
     maxAge: 60 * 60 * 24 * 7,
   });
 }
